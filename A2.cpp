0d0e6943b0d60aabd576e48d6e4cf106cd5f149f
sarvagya80 <adityapathak8081@gmail.com>
2024-12-22 13:15:08 +0530

    A2.cpp

--- A2.cpp
+++ A2.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+int n, count = 0;
+cout << "Enter a positive integer: ";
+cin >> n;
+while (n != 0) {
+n /= 10;
+count++;
+}
+cout << "The total number of digits is: " << count << endl;
+return 0;
+}
